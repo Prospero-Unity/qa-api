93612d47b358e07d2a00f0c151dd8f43be521132
Eric Grochowski <egrochowski42@gmail.com>
2021-11-18 23:05:38 -0500

    Add function to query database for answers

--- server/model/index.js
+++ server/model/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
 const client = require('./../db/postgres/');
 
 module.exports = {
-  getAll: async () => {
+
+  getQuestions: async () => {
     const res = await client.query('SELECT * FROM questions LIMIT 5');
     return res.rows;
+  },
+
+  getAnswers: async() => {
+    const res = await client.query('SELECT * FROM answers LIMIT 5');
+    return res.rows;
   }
+
 }
