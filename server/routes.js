c9b545dcd6026177559eb3283067dd60d3dc306a
Eric Grochowski <egrochowski42@gmail.com>
2021-11-18 23:03:19 -0500

    Add route to get answers

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
-router.get('/api/questions', controller.get);
+router.get('/api/questions', controller.getQuestions);
+router.get('/api/questions/answers', controller.getAnswers);
 
 module.exports = router;
