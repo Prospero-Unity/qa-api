7d1981c3591066aaad7511e892135afed1096d5e
Eric Grochowski <egrochowski42@gmail.com>
2021-11-18 19:46:03 -0500

    Add routes for answers

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
-router.get('/api/products/questions', controller.get);
+router.get('/api/questions', controller.get);
 
 module.exports = router;
