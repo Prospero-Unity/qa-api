26510260b564b673014e998180e1f4e94537ab01
Eric Grochowski <egrochowski42@gmail.com>
2021-11-21 23:09:18 -0500

    Move morgan before parser

--- server/controller/index.js
+++ server/controller/index.js
@@ -2,11 +2,34 @@ const model = require('./../model');
 
 module.exports = {
 
-  getQuestions: async (req, res) => {
-    res.send(await model.getQuestions());
+  getQuestions: (req, res) => {
+    model.getQuestions(req.query)
+      .then(async (questions) => {
+        let answers = {}
+        for (let question of questions) {
+          let result = (await model.getAnswers(question.question_id));
+          for (let answer of result) {
+            answers[answer.answer_id] = answer;
+          }
+          question.answer = answers;
+        }
+        return questions;
+      })
+      .then(results => {
+        res.send(results[0])
+      })
+      .catch(error => {
+        res.status(500).send(error)
+      });
   },
 
-  getAnswers: async (req, res) => {
-    res.send(await model.getAnswers())
+  getAnswers: (req, res) => {
+    model.getAnswers(req.params.question_id)
+      .then(results => {
+        res.send(results);
+      })
+      .catch(error => {
+        res.status(500).send(error)
+      })
   }
 }
--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const morgan = require('morgan');
 const routes = require('./routes.js');
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(routes);
-app.use(morgan('dev'));
 
 app.listen(3000, () => {
   console.log('listening on port 3000...')
