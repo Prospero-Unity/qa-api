a6cad510097dc83e2240fd804b579504a6519e03
Eric Grochowski <egrochowski42@gmail.com>
2021-11-22 20:56:17 -0500

    Remove JOIN from answer query and add getPhotos query

--- server/model/index.js
+++ server/model/index.js
@@ -2,31 +2,33 @@ const client = require('./../db/postgres/');
 
 module.exports = {
 
-  getQuestions: async ({product_id, page, count}) => {
-    return await client.query(`SELECT question_id, product_id, question_body, question_date, asker_name, question_helpfulness, reported FROM questions WHERE product_id = ${product_id} LIMIT ${count}`)
+  getQuestions: async ({product_id, page=1, count=1}) => {
+    return await client.query(
+      `SELECT question_id, question_body, question_date,
+       asker_name, question_helpfulness, reported
+       FROM questions WHERE product_id = ${product_id}
+       OFFSET ${page * count - count}
+       LIMIT ${count}`)
       .then(results =>  results.rows)
       .catch(error => error)
   },
 
-  getAnswers: async (qid) => {
+  getAnswers: async (qid, page=0, count=5) => {
     return await client.query(
-      `SELECT
-        answers.answer_id, answer_date,
-        answer_body, answerer_name, answer_helpfulness,
-        COALESCE (json_agg(json_build_object('id', photo_id, 'url', photos.url))
-        FILTER (WHERE photos.url IS NOT NULL), '[]') AS photos
-      FROM answers
-      LEFT   JOIN photos
-      ON photos.answer_id = answers.answer_id
-      WHERE answers.question_id = ${qid}
-      GROUP BY answers.answer_id
-      LIMIT 5`)
-        .then(results => {
-          return results.rows;
-        })
-        .catch(error => {
-          console.log('err')
-          return error;
-        })
+      `SELECT answer_id, answer_date, answer_body, answerer_name, answer_helpfulness
+       FROM answers WHERE question_id=${qid} LIMIT ${count}`)
+      .then(results => results.rows)
+      .catch(error =>  error);
+  },
+
+  getPhotos: async (answerID) => {
+      return await client.query(
+        `SELECT COALESCE (json_agg(json_build_object('id', photo_id, 'url', url))
+         FILTER (WHERE url IS NOT NULL), '[]') AS photos from photos
+         WHERE answer_id=${answerID}
+         GROUP BY answer_id`)
+      .then(results => results.rows)
+      .catch(error => error)
+
   }
 }
