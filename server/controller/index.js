7d80e0e85b6cd8fc41372eb1d3774a694c41b889
Eric Grochowski <egrochowski42@gmail.com>
2021-11-22 20:55:04 -0500

    Add answer helper function and refactor getAnswer and getQuestion functions

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,35 +1,42 @@
 const model = require('./../model');
 
+async function queryAnswers(qid, page, count) {
+  const answers = await model.getAnswers(qid)
+    .then(results => results)
+    .catch(error => error);
+  for (let answer of answers) {
+    const [photos] = await model.getPhotos(answer.answer_id)
+      .then(photos => photos)
+      .catch(error => error);
+    photos ? answer.photos = photos.photos : answer.photos = [];
+  }
+  return answers;
+}
+
 module.exports = {
 
-  getQuestions: (req, res) => {
-    model.getQuestions(req.query)
-      .then(async (questions) => {
-        let answers = {}
-        for (let question of questions) {
-          let result = (await model.getAnswers(question.question_id));
-          for (let answer of result) {
-            answers[answer.answer_id] = answer;
-          }
-          question.answer = answers;
-        }
-        return questions;
-      })
-      .then(results => {
-        res.send(results[0])
-      })
-      .catch(error => {
-        res.status(500).send(error)
-      });
+  getQuestions: async (req, res) => {
+    try {
+      let obj = {};
+      obj.product_id = req.query.product_id;
+      obj.results = await model.getQuestions(req.query)
+        .then((results) => {return results})
+        .catch(error => error);
+        for (let question of obj.results) {
+          question.answers = await queryAnswers(question.question_id);
+      }
+      res.send(obj);
+    } catch(error) {
+      console.log(error)
+      res.status(500).send(error)
+    }
   },
 
-  getAnswers: (req, res) => {
-    model.getAnswers(req.params.question_id)
-      .then(results => {
-        res.send(results);
-      })
-      .catch(error => {
-        res.status(500).send(error)
-      })
+  getAnswers: async (req, res) => {
+    try {
+      res.send(await queryAnswers(req.params.question_id, req.query.page, req.query.count));
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 }
