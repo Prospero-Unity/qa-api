a95f3f424dc167f27a727cf3ac746af245149947
Eric Grochowski <egrochowski42@gmail.com>
2021-11-22 20:54:38 -0500

    Add compression module

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const compression = require('compression');
 const routes = require('./routes.js');
 const app = express();
 
 app.use(morgan('dev'));
+app.use(compression({
+  threshold: 0,
+  level: 10
+}));
 app.use(express.json());
 app.use(routes);
-
 app.listen(3000, () => {
   console.log('listening on port 3000...')
 });
