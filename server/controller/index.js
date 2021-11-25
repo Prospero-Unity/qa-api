50dddddaadc8f57481b10970c2af1492c9aa9d7d
Eric Grochowski <egrochowski42@gmail.com>
2021-11-25 00:07:49 -0500

    Handle post request to questions

--- server/controller/index.js
+++ server/controller/index.js
@@ -49,5 +49,14 @@ module.exports = {
     } catch (error) {
       res.status(500).send(error);
     }
+  },
+
+  addQuestion: async (req, res) => {
+    try {
+      await(model.addQuestion(req.body));
+      res.sendStatus(201);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 }
