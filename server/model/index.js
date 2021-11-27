a00fb39bc8a2f9afb75b530f65b1bc623080a39a
Eric Grochowski <egrochowski42@gmail.com>
2021-11-27 11:50:11 -0500

    Format questions response object and create helper function for answers query

--- server/model/index.js
+++ server/model/index.js
@@ -13,42 +13,56 @@ async function getPhotos(answerID) {
   }
 }
 
+async function queryAnswers(qid, page=0, count=5) {
+  try {
+    const answers = {};
+    const results = await client.query(
+      `SELECT answer_id, answer_date, answer_body,
+       answerer_name, answer_helpfulness, reported
+       FROM answers WHERE question_id=${qid}
+       OFFSET ${page} FETCH NEXT ${count} ROWS ONLY`);
+    for (let answer of results.rows) {
+      const obj = {};
+      const answerID = answer.answer_id;
+      obj.id = answerID;
+      obj.body = answer.answer_body;
+      obj.date = answer.answer_date;
+      obj.answerer_name = answer.answerer_name;
+      obj.helpfulness = answer.answer_helpfulness;
+      const photos = await getPhotos(answerID);
+      obj.photos = photos;
+      answers[answerID] = obj;
+    }
+    return answers;
+  } catch (error) {
+    return error;
+  }
+}
+
 module.exports = {
 
   getQuestions: async ({product_id, page=0, count=5}) => {
     try {
+      const questions = {};
       const results = await client.query(
         `SELECT question_id, question_body, question_date,
         asker_name, question_helpfulness, reported
         FROM questions WHERE product_id = ${product_id}
         OFFSET ${page} FETCH NEXT ${count} ROWS ONLY`);
-      return results.rows;
+      questions.product_id = product_id;
+      questions.results = results.rows;
+      for (let question of questions.results) {
+        question.answers = await queryAnswers(question.question_id);
+      }
+      return questions;
     } catch (error) {
       return error;
     }
   },
 
-  getAnswers: async (qid, page=0, count=5) => {
+  getAnswers: (qid, page, count) => {
     try {
-      const results = await client.query(
-      `SELECT answer_id, answer_date, answer_body,
-       answerer_name, answer_helpfulness, reported
-       FROM answers WHERE question_id=${qid}
-       OFFSET ${page} FETCH NEXT ${count} ROWS ONLY`);
-
-       const answers = {};
-       for (let answer of results.rows) {
-        const obj = {};
-        const answerID = answer.answer_id;
-        obj.id = answerID;
-        obj.body = answer.answer_body;
-        obj.date = answer.answer_date;
-        obj.answerer_name = answer.answerer_name;
-        obj.helpfulness = answer.answer_helpfulness;
-        const photos = await getPhotos(answerID);
-        obj.photos = photos;
-        answers[answerID] = obj;
-      }
+      const answers = queryAnswers(qid, page, count);
       return answers;
     } catch (error) {
       return error;
