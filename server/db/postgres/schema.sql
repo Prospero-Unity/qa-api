d23ed949c41071814f4993a8cf30aefdf701fc3a
Eric Grochowski <egrochowski42@gmail.com>
2021-11-26 23:29:40 -0500

    Change id data types, change sequence start and create indexes

--- server/db/postgres/schema.sql
+++ server/db/postgres/schema.sql
@@ -3,7 +3,7 @@
 /* USE SDC */
 
 CREATE TABLE questions(
-  question_id integer NOT NULL DEFAULT 0,
+  question_id SERIAL NOT NULL,
   question_body text NOT NULL,
   question_date double precision,
   asker_name text NOT NULL,
@@ -16,12 +16,12 @@ CREATE TABLE questions(
 
 /* Table 'answers' */
 CREATE TABLE answers(
-  answer_id integer NOT NULL,
+  answer_id SERIAL NOT NULL,
   answer_date double precision,
   answer_body text NOT NULL,
   answerer_name text NOT NULL,
   answerer_email text NOT NULL,
-  reported boolean NOT NULL,
+  reported boolean NOT NULL DEFAULT false,
   answer_helpfulness integer DEFAULT 0,
   question_id integer NOT NULL,
   PRIMARY KEY(answer_id)
@@ -29,7 +29,7 @@ CREATE TABLE answers(
 
 /* Table 'photos' */
 CREATE TABLE photos(
-  photo_id integer NOT NULL,
+  photo_id SERIAL NOT NULL,
   answer_id integer NOT NULL,
   url text NOT NULL,
   PRIMARY KEY(photo_id)
@@ -44,6 +44,32 @@ ALTER TABLE answers
 ALTER TABLE photos
   ADD CONSTRAINT answers_photos
     FOREIGN KEY (answer_id) REFERENCES answers (answer_id);
+
+/* Command to get sequence command */
+-- select (pg_get_serial_sequence('questions', 'question_id'));
+
+/* Start Serialization from the last question ID plus one */
+/* number determined by running SELECT COUNT(*) FROM questions; */
+ALTER SEQUENCE questions_question_id_seq RESTART WITH 3518964;
+
+/* Command to get sequence command */
+/* select (pg_get_serial_sequence('answers', 'answers_id')); */
+
+/* Start Serialization from the last question ID plus one */
+/* number determined by running SELECT COUNT(*) FROM anwswers; */
+ALTER SEQUENCE answers_answer_id_seq RESTART WITH 6879307;
+
+/* Command to get sequence command */
+/* select (pg_get_serial_sequence('photos', 'photos_id')); */
+
+/* Start Serialization from the last question ID plus one */
+/* number determined by running SELECT COUNT(*) FROM photos; */
+ALTER SEQUENCE photos_photo_id_seq RESTART WITH 2063760;
+
+
+CREATE INDEX photos_index ON photos(answer_id, url);
+CREATE INDEX answer_index ON answers(question_id);
+CREATE INDEX question_index ON questions(product_id);
 
 /* Copy data to tables */
 -- id, product_id, body, date_written, asker_name, asker_email, reported, helpful
