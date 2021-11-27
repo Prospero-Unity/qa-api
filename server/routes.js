ef9ae0dccc38a996070b6b660424b49cce4fc7c5
Eric Grochowski <egrochowski42@gmail.com>
2021-11-27 11:54:01 -0500

    Add route to post answers

--- server/routes.js
+++ server/routes.js
@@ -4,5 +4,6 @@ const controller = require('./controller');
 router.get('/qa/questions/', controller.getQuestions);
 router.get('/qa/questions/:question_id/answers', controller.getAnswers);
 router.post('/qa/questions', controller.addQuestion);
+router.post('/qa/questions/:question_id/answers', controller.addAnswer);
 
 module.exports = router;
