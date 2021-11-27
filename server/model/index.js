4a16542bdd0a0bb8f7b95033cb3d8cc3366b349f
Eric Grochowski <egrochowski42@gmail.com>
2021-11-27 13:33:19 -0500

    Create function to post answers to db

--- server/model/index.js
+++ server/model/index.js
@@ -6,7 +6,8 @@ async function getPhotos(answerID) {
       `SELECT COALESCE (json_agg(json_build_object('id', photo_id, 'url', url))
         FILTER (WHERE url IS NOT NULL), '[]') AS photos from photos
         WHERE answer_id=${answerID}
-        GROUP BY answer_id`);
+        GROUP BY answer_id`
+    );
     return results.rows[0] ? results.rows[0].photos : [];
   } catch (error) {
     return error;
@@ -20,7 +21,8 @@ async function queryAnswers(qid, page=0, count=5) {
       `SELECT answer_id, answer_date, answer_body,
        answerer_name, answer_helpfulness, reported
        FROM answers WHERE question_id=${qid}
-       OFFSET ${page} FETCH NEXT ${count} ROWS ONLY`);
+       OFFSET ${page} FETCH NEXT ${count} ROWS ONLY`
+    );
     for (let answer of results.rows) {
       const obj = {};
       const answerID = answer.answer_id;
@@ -48,7 +50,8 @@ module.exports = {
         `SELECT question_id, question_body, question_date,
         asker_name, question_helpfulness, reported
         FROM questions WHERE product_id = ${product_id}
-        OFFSET ${page} FETCH NEXT ${count} ROWS ONLY`);
+        OFFSET ${page} FETCH NEXT ${count} ROWS ONLY`
+      );
       questions.product_id = product_id;
       questions.results = results.rows;
       for (let question of questions.results) {
@@ -75,7 +78,27 @@ module.exports = {
         `INSERT INTO questions
           (question_body, asker_name, asker_email, product_id)
          VALUES
-           ('${question_body}', '${asker_name}', '${asker_email}', ${product_id})`);
+           ('${question_body}', '${asker_name}', '${asker_email}', ${product_id})`
+      );
+    } catch (error) {
+      return error;
+    }
+  },
+
+  addAnswer: async (qid, {answer_body, answerer_name, answerer_email, photos}) => {
+    try {
+      const answerResult = await client.query(
+        `INSERT INTO answers(question_id, answer_body, answerer_name, answerer_email)
+         VALUES('${qid}', '${answer_body}', '${answerer_name}', '${answerer_email}')
+         RETURNING answer_id`
+      );
+      const answerID = answerResult.rows[0].answer_id;
+      for (let url of photos) {
+        await client.query(
+          `INSERT INTO photos(answer_id, url)
+           VALUES('${answerID}', '${url}')`
+        );
+      }
     } catch (error) {
       return error;
     }
