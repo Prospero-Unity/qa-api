bf836818cbbe01b6145f7a234175ade08151dcaf
Eric Grochowski <egrochowski42@gmail.com>
2021-11-29 13:51:40 -0500

    Create route to report answers

--- server/routes.js
+++ server/routes.js
@@ -8,5 +8,6 @@ router.post('/qa/questions/:question_id/answers', controller.addAnswer);
 router.put('/qa/questions/:question_id/helpful', controller.markQuestionHelpful);
 router.put('/qa/answers/:answer_id/helpful', controller.markAnswerHelpful);
 router.put('/qa/questions/:question_id/report', controller.reportQuestion);
+router.put('/qa/answers/:answer_id/report', controller.reportAnswer);
 
 module.exports = router;
