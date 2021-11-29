941d8c6f580656a2ee7e871407ff1d44fac2b11c
Eric Grochowski <egrochowski42@gmail.com>
2021-11-29 12:39:26 -0500

    Create route to mark answer as helpful

--- server/routes.js
+++ server/routes.js
@@ -6,5 +6,6 @@ router.get('/qa/questions/:question_id/answers', controller.getAnswers);
 router.post('/qa/questions', controller.addQuestion);
 router.post('/qa/questions/:question_id/answers', controller.addAnswer);
 router.put('/qa/questions/:question_id/helpful', controller.markQuestionHelpful);
+router.put('/qa/questions/:answer_id/helpful', controller.markAnswerHelpful);
 
 module.exports = router;
