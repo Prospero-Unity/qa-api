7075dc1bde26a436ef6462f2cecf9465cdb1bb04
Eric Grochowski <egrochowski42@gmail.com>
2021-11-28 21:04:31 -0500

    Fix error response handlers

--- server/controller/index.js
+++ server/controller/index.js
@@ -4,41 +4,59 @@ module.exports = {
 
   getQuestions: async (req, res) => {
     try {
-      res.setHeader('Cache-Control', 'public', 'max-age=604800');
-      res.send(await model.getQuestions(req.query));
-    } catch(error) {
-      res.status(500).send(error)
+      const questions = await model.getQuestions(req.query);
+      if (questions instanceof Error) {
+        throw questions;
+      } else {
+        res.setHeader('Cache-Control', 'public', 'max-age=604800');
+        res.send(questions);
+      }
+    } catch (error) {
+      res.status(500).send(error.message);
     }
   },
 
   getAnswers: async (req, res) => {
     try {
-      res.setHeader('Cache-Control', 'public', 'max-age=604800');
-      res.send(await model.getAnswers(
-          req.params.question_id,
-          req.query.page,
-          req.query.count
-      ));
+      const answers = await model.getAnswers(
+        req.params.question_id,
+        req.query.page,
+        req.query.count
+      );
+      if (answers instanceof Error) {
+        throw answers;
+      } else {
+        res.setHeader('Cache-Control', 'public', 'max-age=604800');
+        res.send(answers);
+      }
     } catch (error) {
-      res.status(500).send(error);
+      res.status(500).send(error.message);
     }
   },
 
   addQuestion: async (req, res) => {
     try {
-      await(model.addQuestion(req.body));
-      res.sendStatus(201);
+      const response = await(model.addQuestion(req.body));
+      if (response instanceof Error) {
+        throw response;
+      } else {
+        res.sendStatus(201);
+      }
     } catch (error) {
-      res.status(500).send(error);
+      res.status(500).send(error.message);
     }
   },
 
   addAnswer: async (req, res) => {
     try {
-      await(model.addAnswer(req.params.question_id, req.body));
-      res.sendStatus(201);
+      const response = await(model.addAnswer(req.params.question_id, req.body));
+      if (response instanceof Error) {
+        throw response;
+      } else {
+        res.sendStatus(201);
+      }
     } catch (error) {
-      res.status(500).send(error.toString());
+      res.status(500).send(error.message);
     }
   }
 }
