28cd850f14f452de41244eacc41eada60f95ebe8
Eric Grochowski <egrochowski42@gmail.com>
2021-12-02 21:56:02 -0500

    Update route

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ router.get('/qa/questions/', controller.getQuestions);
 router.get('/qa/questions/:question_id/answers', controller.getAnswers);
 router.get('/loaderio-f4c20886578f17a9434bec988399bb5e', (req, res) => {
 	res.send('loaderio-f4c20886578f17a9434bec988399bb5e');
-}); 
+});
 router.post('/qa/questions', controller.addQuestion);
 router.post('/qa/questions/:question_id/answers', controller.addAnswer);
 router.put('/qa/questions/:question_id/helpful', controller.markQuestionHelpful);
